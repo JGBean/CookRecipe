2e1f3908757e1c245f5d365e347bd250afa1be0b
suuu <suuuhyuni@naver.com>
2024-10-27 16:43:50 +0900

    splash, navbar 생성

--- app/src/main/java/com/cook_recipe_app/firebase/MainActivity.kt
+++ app/src/main/java/com/cook_recipe_app/firebase/MainActivity.kt
@@ -1,13 +1,14 @@
-package com.cook_recipe_app.firebase
+package com.cook_recipe_app
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.cook_recipe_app.firebase.R
 import com.cook_recipe_app.firebase.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,17 +20,40 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        // binding을 사용해 BottomNavigationView에 리스너 설정
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            val selectedFragment: Fragment = when (item.itemId) {
+                R.id.nav_recipe -> RecipeFragment()
+                R.id.nav_community -> CommunityFragment()
+                R.id.nav_user -> MypageFragment()
+                else -> RecipeFragment()  // 기본 선택
+            }
+            // Fragment 전환
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, selectedFragment).commit()
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+            true // 아이템 선택이 처리되었음을 나타냄
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        return when (item.itemId) {
+//            R.id.action_settings -> true
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
