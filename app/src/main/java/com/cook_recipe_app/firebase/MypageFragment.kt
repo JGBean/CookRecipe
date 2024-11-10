286332fb67442bc26807c255fac6bb260e988930
misterraba <juhyun7306@gmail.com>
2024-11-11 08:45:16 +0900

    마이페이지 화면입니다.

--- app/src/main/java/com/cook_recipe_app/firebase/MypageFragment.kt
+++ app/src/main/java/com/cook_recipe_app/firebase/MypageFragment.kt
@@ -6,34 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.cook_recipe_app.firebase.R
+import com.cook_recipe_app.firebase.databinding.FragmentMypageBinding
 
 
 class MypageFragment : BaseFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-    }
+    var binding: FragmentMypageBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mypage, container, false)
+        binding = FragmentMypageBinding.inflate(inflater)
+        return binding?.root
     }
 
-//    companion object {
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            MypageFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
