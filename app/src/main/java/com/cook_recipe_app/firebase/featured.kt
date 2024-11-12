d1d0a74311018f1e6979a75e0c210613a67c3adc
misterraba <juhyun7306@gmail.com>
2024-11-12 11:53:07 +0900

    마이페이지 화면수정 + 즐겨찾기 화면 추가했습니다.

--- app/src/main/java/com/cook_recipe_app/firebase/featured.kt
+++ app/src/main/java/com/cook_recipe_app/firebase/featured.kt
@@ -5,30 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.cook_recipe_app.firebase.R
-import com.cook_recipe_app.firebase.databinding.FragmentMypageBinding
+import androidx.navigation.fragment.findNavController
+import com.cook_recipe_app.firebase.databinding.FragmentFeaturedBinding
 
 
-class MypageFragment : BaseFragment() {
 
-    var binding: FragmentMypageBinding? = null
+class featured : BaseFragment() {
+
+    var binding: FragmentFeaturedBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMypageBinding.inflate(inflater)
+        binding = FragmentFeaturedBinding.inflate(inflater)
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding?.ToFeatured?.setOnClickListener{
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, featured())
-                .addToBackStack(null)
-                .commit()
-        }
     }
 }
